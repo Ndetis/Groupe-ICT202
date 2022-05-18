cdd62e302ea61a8cb91b210f78503cd9b9ef3fe7
Ndetis Elie <nyobedjobelie@gmail.com>
2022-05-18 12:45:46 +0100

    first changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular-TP'`, () => {
+  it(`should have as title 'Mon-CV'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular-TP');
+    expect(app.title).toEqual('Mon-CV');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Angular-TP app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Mon-CV app is running!');
   });
 });
